ff813bea502e8a619a8d98ea81c3310c652f2e33
Brett Tanner <bretttanner171@gmail.com>
2022-11-25 12:44:30 +0900

    Add form for Cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,8 +1,58 @@
 class CarsController < ApplicationController
 
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      flash.now[:alert] = "Couldn't create the car"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+
+    if @car.update(car_params)
+      flash[:notice] = "Car updated successfully"
+      redirect_to car_path(@car)
+    else
+      flash.now[:alert] = "Unable to update car"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    
+    if @car.destroy
+      flash[:notice] = "Car crashed successfully"
+      redirect_to cars_path
+    else
+      flash.now[:alert] = "Failed to crash car"
+    end
+  end
+
   private
 
   def car_params
-    params.require(:car).permit(:brand, :model, variant_attributes: [:type, :top_speed, :price, :car_id])
+    params.require(:car).permit(:brand, :model, variants_attributes: [:variation, :top_speed, :price])
   end
 end
